fecdd71019c065708cb7f83941d3275376a2674d
imontalvomiguel <ubaldo.im@gmail.com>
2018-11-11 18:28:50 -0600

    Sketch home page

--- spec/acceptance/subscriptions_app_spec.rb
+++ spec/acceptance/subscriptions_app_spec.rb
@@ -35,5 +35,34 @@ module Subscriptions
         expect(shop.token).to eq('token')
       end
     end
+
+    context 'when session exists' do
+      let(:rack_env) {
+        {
+          'rack.session' => {
+            :shop => 'snowdevil.myshopify.com',
+            :token => 'token'
+          }
+        }
+      }
+
+      it 'allows to access the home page' do
+        pending 'Need to protect the home page'
+
+        get '/', {}, rack_env
+
+        expect(last_response).to be_ok
+      end
+    end
+
+    context 'when session does not exist' do
+      it 'does not allow to access the home page' do
+        pending 'Need to protect the home page'
+
+        get '/'
+
+        expect(last_response.status).not_to be_ok
+      end
+    end
   end
 end
